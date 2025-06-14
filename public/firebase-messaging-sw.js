f15590244b25397a21f9bc300536d0bcda4bcb7b
usman <usmanbalogun044@gmail.com>
2025-06-14 10:57:49 +0100

    new push
    new push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,15 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 // // Initialize the Firebase app in the service worker by passing the generated config
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA6otyfQwR84kbBN3m7AX7_srmA4wS2VU8",
-    authDomain: "koka-d79bc.firebaseapp.com",
-    projectId: "koka-d79bc",
-    storageBucket: "koka-d79bc.firebasestorage.app",
-    messagingSenderId: "982803285281",
-    appId: "1:982803285281:web:4a478f681dc5240ba46a60",
-    measurementId: "G-5JRMVR0VJP"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 firebase?.initializeApp(firebaseConfig)
